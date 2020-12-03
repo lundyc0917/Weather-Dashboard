6108c8ffaf41bc90a4ea0ebfea0376a1c7055730
lundyc0917 <lundyc0917@gmail.com>
2020-12-03 18:58:00 -0500

    added API

---
+++ Assets/js/script.js
@@ -0,0 +1,23 @@
+var cityNameEl = document.getElementById("cityName");
+var searchEl = document.getElementById("searchBtn")
+var apiKey = "a5599c7df51f4d43c94489d4ea681d06";
+
+
+
+
+function returnWeather(){
+    cityNameEl = $("#cityName").val().trim();
+
+    console.log(cityNameEl);
+
+    var cityURL = "https://api.openweathermap.org/data/2.5/weather?id="+cityNameEl+"&appid="+apiKey;
+    console.log(cityURL);
+    return cityURL;  
+}
+
+
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var queryURL = returnWeather();
+  });
