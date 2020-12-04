0354e1303fe4b2e8fc2c1a71afe7691540ab7ea4
lundyc0917 <lundyc0917@gmail.com>
2020-12-03 23:24:23 -0500

    return current weather with API

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,40 @@
-var cityNameEl = document.getElementById("cityName");
 var searchEl = document.getElementById("searchBtn")
 var apiKey = "a5599c7df51f4d43c94489d4ea681d06";
 
 
 
-
+// function to return the url to find the city.
 function returnWeather(){
-    cityNameEl = $("#cityName").val().trim();
+    var cityName = $("#cityName").val().trim();
 
-    console.log(cityNameEl);
+    console.log(cityName);
 
-    var cityURL = "https://api.openweathermap.org/data/2.5/weather?id="+cityNameEl+"&appid="+apiKey;
+    var cityURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=imperial&appid="+apiKey;
     console.log(cityURL);
-    return cityURL;
+   
+    $.ajax({
+        url: cityURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+        var temp = $("<p>").text(response.main.temp);
+        $("#temp").append(temp);
+        var humidity = $("<p>").text(response.main.humidity);
+        $("#humidity").append(humidity);
+        var windSpeed = $("<p>").text(response.wind.speed);
+        $("#wind").append(windSpeed);
+    });
+
+
+
+    
 }
 
-
-
+// function to run once the search button is clicked
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
     var searchURL = returnWeather();
-    console.log(searchURL);
     return searchURL;
 
   });
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Weather-Dashboard
 Weather Dashboard to view weather in a specific city.
+
+## Link to Site
+https://lundyc0917.github.io/Weather-Dashboard/
+
+## Motivation
+Create a site where users can go and search for a city and see the current weather as well as the forecast for the next 5 days.
+
+## Build Status
+build | in-progress
+
+## Screen Shot
+![empty_screenshot](https://user-images.githubusercontent.com/71233342/101119114-9f6e6980-35b8-11eb-8f8c-040240027d19.png)
+
+
+## Framework
+
+Built Using:
+ - JavaScript
+ - HTML
+ - Bootstrap
+
+## Features
+
+When the user enters a city name and hits "search", they are shown the current temperature, humidity, wind speed, uv index, and 5-day forecast for the selected city.
+
+## Code Example
+
+.js code
+
+`````````````````````````
+function returnWeather(){
+    cityNameEl = $("#cityName").val().trim();
+
+    console.log(cityNameEl);
+
+    var cityURL = "https://api.openweathermap.org/data/2.5/weather?id="+cityNameEl+"&appid="+apiKey;
+    console.log(cityURL);
+    return cityURL;
+}
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var searchURL = returnWeather();
+    console.log(searchURL);
+    return searchURL;
+
+  });
+`````````````````````````
